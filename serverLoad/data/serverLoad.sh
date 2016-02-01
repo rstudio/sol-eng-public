cfa0ee66d24d0220f14cb877dd87aeb77e8f9679
Nathan Stephens <nwstephens@gmail.com>
2016-02-01 00:37:42 +0000

    minor note for linux dependencies

--- serverLoad/data/serverLoad.sh
+++ serverLoad/data/serverLoad.sh
@@ -9,7 +9,7 @@ LOAD=`uptime | sed 's/.*load average: //' | awk -F\, '{print $1}'`
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 echo "$DATE,$LOAD" >> $MYDIR/$FILENAME
 
-# Write load data to database
+# Write load data to database (requires linux packages: sqlite3 libsqlite3-dev)
 #DBNAME=$MYDIR/$TABLENAME.db
 #sqlite3 $DBNAME "create table if not exists $TABLENAME (id INTEGER PRIMARY KEY ASC, dte TEXT, Load REAL);"
 #sqlite3 $DBNAME "insert into $TABLENAME (dte, Load) values ('$DATE','$LOAD');"
